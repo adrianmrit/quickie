b6f1b78a183d65c6d78ec5495e1145452e8ecc1f
Adrian Martinez Rodriguez <adrianmrit@gmail.com>
2024-08-16 00:37:45 -0400

    build: release 0.2.0

--- src/quickie/_meta.py
+++ src/quickie/_meta.py
@@ -1,6 +1,6 @@
 """Metadata for the package."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Adrian Martinez Rodriguez"
 __email__ = "adrianmrit@gmail.com"
 __home__ = "https://github.com/adrianmrit/quickie"
